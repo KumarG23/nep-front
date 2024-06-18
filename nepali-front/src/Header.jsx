7fad404cbccd74ea7e6705310a0cd9d936c071eb
Neal Sharma <sharma.neal@yahoo.com>
2024-06-18 14:22:54 -0400

    made mobile friendly

--- nepali-front/src/Header.jsx
+++ nepali-front/src/Header.jsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaBars } from 'react-icons/fa'; // Add react-icons for the hamburger icon
 import Logo from './assets/logo.webp';
-
+import './Header.css'; // Import custom CSS for additional styling
 
 const Header = () => {
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => setExpanded(!expanded);
+  const closeMenu = () => setExpanded(false);
+
+
+
   return (
-    <Navbar expand="md" className="navbar-light sticky-top nav-bar" style={{ height: '100px' }}>
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          <img src={Logo} alt="Logo" className="rounded float-start" id="logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav">
+    <Navbar expand="md" expanded={expanded} className="navbar-light sticky-top nav-bar" style={{ height: '100px' }}>
+      <Container className="d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+          <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+            <img src={Logo} alt="Logo" className="rounded float-start" id="logo" />
+            <span id="title">Nepali Threads</span>
+          </Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls="navbar-nav" onClick={handleToggle}>
           <FaBars /> {/* Use the FaBars icon for the hamburger menu */}
         </Navbar.Toggle>
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/" className="nav-item nav-link" id="navitem">Home</Nav.Link>
-            <Nav.Link as={Link} to="/login" className="nav-item nav-link" id="navitem">Login</Nav.Link>
-            <Nav.Link as={Link} to="/profile" className="nav-item nav-link" id="navitem">Profile</Nav.Link>
-            <Nav.Link as={Link} to="/product" className="nav-item nav-link" id="navitem">Shop</Nav.Link>
-            <Nav.Link as={Link} to="/cart" className="nav-item nav-link" id="navitem">Cart</Nav.Link>
+        <Navbar.Collapse id="navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link as={Link} to="/" className="nav-item nav-link" id="navitem" onClick={closeMenu}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/login" className="nav-item nav-link" id="navitem" onClick={closeMenu}>Login</Nav.Link>
+            <Nav.Link as={Link} to="/profile" className="nav-item nav-link" id="navitem" onClick={closeMenu}>Profile</Nav.Link>
+            <Nav.Link as={Link} to="/product" className="nav-item nav-link" id="navitem" onClick={closeMenu}>Shop</Nav.Link>
+            <Nav.Link as={Link} to="/cart" className="nav-item nav-link" id="navitem" onClick={closeMenu}>Cart</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -30,3 +41,4 @@ const Header = () => {
 }
 
 export default Header;
+
