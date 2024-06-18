2319a40f8c23a3972bfff9b93427857ea1960325
Neal Sharma <sharma.neal@yahoo.com>
2024-06-18 13:12:54 -0400

    navbar adjustments

--- nepali-front/src/Header.jsx
+++ nepali-front/src/Header.jsx
@@ -1,25 +1,32 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaBars } from 'react-icons/fa'; // Add react-icons for the hamburger icon
 import Logo from './assets/logo.webp';
-import {Button, Container, Navbar, Modal} from 'react-bootstrap';
+import './Header.css'; // Import custom CSS for additional styling
 
-import React from 'react'
-
-export const Header = () => {
+const Header = () => {
   return (
-    <Navbar id='navbar' className='navbar navbar-expand-md navbar-light sticky-top nav-bar' style={{ height: '100px' }}>
-        <div id='navcontainer' className='container d-flex flex-row'>
-        <img src={Logo} alt='Logo' className='rounded float-start' id='logo'/>
-        <span className='mx-auto' id='title'>Nepali Threads</span>
-        <div className='navbar-nav'>
-        <Link to='/' className='nav-item nav-link' id='navitem'>Home</Link>
-        <Link to='/login' className='nav-item nav-link' id='navitem'>Login</Link>
-        <Link to='/profile' className='nav-item nav-link' id='navitem'>Profile</Link>
-        <Link to='/product' className='nav-item nav-link' id='navitem'>Shop</Link>
-        <Link to='/cart' className='nav-item nav-link' id='navitem'>Cart</Link>
-        </div>
-        </div>
+    <Navbar expand="md" className="navbar-light sticky-top nav-bar" style={{ height: '100px' }}>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img src={Logo} alt="Logo" className="rounded float-start" id="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav">
+          <FaBars /> {/* Use the FaBars icon for the hamburger menu */}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/" className="nav-item nav-link" id="navitem">Home</Nav.Link>
+            <Nav.Link as={Link} to="/login" className="nav-item nav-link" id="navitem">Login</Nav.Link>
+            <Nav.Link as={Link} to="/profile" className="nav-item nav-link" id="navitem">Profile</Nav.Link>
+            <Nav.Link as={Link} to="/product" className="nav-item nav-link" id="navitem">Shop</Nav.Link>
+            <Nav.Link as={Link} to="/cart" className="nav-item nav-link" id="navitem">Cart</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
-  )
+  );
 }
 
-export default Header
+export default Header;
