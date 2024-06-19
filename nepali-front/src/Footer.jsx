af9f809a974b84e187f8cb72e6c9e1f34d4251f0
Neal Sharma <sharma.neal@yahoo.com>
2024-06-19 14:56:39 -0400

    added footer

--- nepali-front/src/Footer.jsx
+++ nepali-front/src/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import nepaliFlag from './assets/nepalFlag.png';
 
 export const Footer = () => {
   return (
-    <div>
-        
-    </div>
+    <footer>
+      <p id='threads'>© 2024 Nepali Threads</p>
+      <p id='threads'>Site created by Neal Sharma</p>
+      <a href='www.linkedin.com/in/neal-sharma23' target='_blank' rel='noopener noreferrer' id='linked'>Linkedin Profile</a>
+      <img className='flag' src={nepaliFlag} alt='Nepali Flag' />
+    </footer>
   )
 }
